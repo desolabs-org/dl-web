384116c0fd914045f7750038b1f559dcb9a7c3b6
kanshi <4039242-_pdv_@users.noreply.gitlab.com>
2022-10-16 15:52:29 +0200

    Recognition awards video

--- src/pages/landing-page/RecognitionCard.js
+++ src/pages/landing-page/RecognitionCard.js
@@ -1,14 +1,14 @@
-import { Container, Box } from '@chakra-ui/react'
-import { VStack, HStack } from '@chakra-ui/react'
-import { Heading, Text, Button, Image } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
+import { VStack } from '@chakra-ui/react'
+import { Heading, Text, } from '@chakra-ui/react'
 
 export default function RecognitionCard(props) {
     return (
 	<Container centerContent>
         <VStack spacing='2rem'>
-            <Box>
-              <Image src='/reward.png' height='200px'/>
-            </Box>
+            <Container centerContent>
+                <iframe width='356px' height='200px' src="https://www.youtube.com/embed/y4851eQMb-A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Container>
             <VStack>
                 <Heading>
                     Recognition awards
