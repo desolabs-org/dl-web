cbe798ed5e5dd3a0b90781e0f6830b2b50601eeb
kanshi <4039242-_pdv_@users.noreply.gitlab.com>
2022-11-16 17:22:52 +0100

    dummy commit

--- src/pages/hackathon/PrizePool.jsx
+++ src/pages/hackathon/PrizePool.jsx
@@ -25,9 +25,6 @@ class PrizePool extends React.Component {
         })
         .then(res => res.json())
         .then(resultFn,
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
             (error) => {
                 this.setState({ isLoaded: true, error });
             }
